77348d923f8542199eb460c8204cd0a421c38be4
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2023-12-23 16:36:06 +0100

    some fixes

--- src/NBeats.jl
+++ src/NBeats.jl
@@ -4,7 +4,6 @@ using Flux
 using LinearAlgebra
 
 abstract type AbstractBlock end
-abstract type AbstractBasis end
 
 export Block
 include("block.jl")
--- src/block.jl
+++ src/block.jl
@@ -43,44 +43,30 @@ function (block::NBeatsBlock)(x::AbstractArray)
     return block.basis_function(basis_parameters)
 end
 
-struct GenericBasis <: AbstractBasis
-    backcast_size::Int
-    forecast_size::Int
+# GenericBasis Function
+function generic_basis(backcast_size::Int, forecast_size::Int)
+    return function(theta::AbstractArray)
+        backcast = theta[:, 1:backcast_size]
+        forecast = theta[:, end-forecast_size+1:end]
+        return backcast, forecast
+    end
 end
 
-function (basis::GenericBasis)(theta::AbstractArray)
-    backcast = theta[:, 1:basis.backcast_size]
-    forecast = theta[:, end-basis.forecast_size+1:end]
-    return backcast, forecast
-end
-
-struct TrendBasis <: AbstractBasis
-    polynomial_size::Int
-    backcast_time::Array{Float32, 2}
-    forecast_time::Array{Float32, 2}
-end
-
-function TrendBasis(degree_of_polynomial::Int, backcast_size::Int, forecast_size::Int)
+# TrendBasis Function
+function trend_basis(degree_of_polynomial::Int, backcast_size::Int, forecast_size::Int)
     polynomial_size = degree_of_polynomial + 1
     backcast_time = hcat([Float32.((0:backcast_size-1) / backcast_size) .^ i for i in 0:degree_of_polynomial]...)
     forecast_time = hcat([Float32.((0:forecast_size-1) / forecast_size) .^ i for i in 0:degree_of_polynomial]...)
-    TrendBasis(polynomial_size, backcast_time, forecast_time)
+
+    return function(theta::AbstractArray)
+        backcast = theta[:, polynomial_size+1:end] * backcast_time
+        forecast = theta[:, 1:polynomial_size] * forecast_time
+        return backcast, forecast
+    end
 end
 
-function (basis::TrendBasis)(theta::AbstractArray)
-    backcast = theta[:, basis.polynomial_size+1:end] * basis.backcast_time
-    forecast = theta[:, 1:basis.polynomial_size] * basis.forecast_time
-    return backcast, forecast
-end
-
-struct SeasonalityBasis <: AbstractBasis
-    backcast_cos_template::Array{Float32, 2}
-    backcast_sin_template::Array{Float32, 2}
-    forecast_cos_template::Array{Float32, 2}
-    forecast_sin_template::Array{Float32, 2}
-end
-
-function SeasonalityBasis(harmonics::Int, backcast_size::Int, forecast_size::Int)
+# SeasonalityBasis Function
+function seasonality_basis(harmonics::Int, backcast_size::Int, forecast_size::Int)
     frequency = vcat(zeros(1, Float32), collect(1:harmonics/2*forecast_size) / harmonics)
 
     backcast_grid = -2π * (collect(0:backcast_size-1) / forecast_size) * frequency'
@@ -91,18 +77,17 @@ function SeasonalityBasis(harmonics::Int, backcast_size::Int, forecast_size::Int
     forecast_cos_template = cos.(forecast_grid)
     forecast_sin_template = sin.(forecast_grid)
 
-    SeasonalityBasis(backcast_cos_template, backcast_sin_template, forecast_cos_template, forecast_sin_template)
+    return function(theta::AbstractArray)
+        params_per_harmonic = size(theta, 2) ÷ 4
+        backcast_harmonics_cos = theta[:, 2 * params_per_harmonic + 1:3 * params_per_harmonic] * backcast_cos_template
+        backcast_harmonics_sin = theta[:, 3 * params_per_harmonic + 1:end] * backcast_sin_template
+        backcast = backcast_harmonics_sin + backcast_harmonics_cos
+
+        forecast_harmonics_cos = theta[:, 1:params_per_harmonic] * forecast_cos_template
+        forecast_harmonics_sin = theta[:, params_per_harmonic + 1:2 * params_per_harmonic] * forecast_sin_template
+        forecast = forecast_harmonics_sin + forecast_harmonics_cos
+
+        return backcast, forecast
+    end
 end
 
-function (basis::SeasonalityBasis)(theta::AbstractArray)
-    params_per_harmonic = size(theta, 2) ÷ 4
-    backcast_harmonics_cos = theta[:, 2 * params_per_harmonic + 1:3 * params_per_harmonic] * basis.backcast_cos_template
-    backcast_harmonics_sin = theta[:, 3 * params_per_harmonic + 1:end] * basis.backcast_sin_template
-    backcast = backcast_harmonics_sin + backcast_harmonics_cos
-
-    forecast_harmonics_cos = theta[:, 1:params_per_harmonic] * basis.forecast_cos_template
-    forecast_harmonics_sin = theta[:, params_per_harmonic + 1:2 * params_per_harmonic] * basis.forecast_sin_template
-    forecast = forecast_harmonics_sin + forecast_harmonics_cos
-
-    return backcast, forecast
-end
