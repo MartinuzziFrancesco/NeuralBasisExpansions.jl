c34417d01fbd6f274a5e8b12b57511ca494ec257
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2024-01-12 20:03:31 +0100

    restructure

--- src/NBeats.jl
+++ src/NBeats.jl
@@ -8,25 +8,8 @@ abstract type AbstractBlock end
 export NBeatsBlock, NBeatsNet
 export GenericBasis, TrendBasis, SeasonalityBasis
 
-struct NBeatsNet
-    blocks::Vector{<:Any}
-end
-
-function (model::NBeatsNet)(x::AbstractArray, input_mask::AbstractArray)
-    residuals = reverse(x; dims=2)
-    input_mask = reverse(input_mask; dims=2)
-    forecast = x[:, end:end]
-
-    for block in model.blocks
-        backcast, block_forecast = block(residuals)
-        residuals = (residuals - backcast) .* input_mask
-        forecast = forecast + block_forecast
-    end
-
-    return forecast
-end
-
-include("block.jl")
 include("basis.jl")
+include("flux/block.jl")
+include("flux/net.jl")
 
 end
